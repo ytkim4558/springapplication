1aa6dabfca9210b59c3acd226cfc4012d53f047c
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-20 22:09:54 +0900

    ApplicationRunner test

--- src/main/java/me/facemoktak/springinitapptest/SampleListener.java
+++ src/main/java/me/facemoktak/springinitapptest/SampleListener.java
@@ -1,13 +1,15 @@
 package me.facemoktak.springinitapptest;
 
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleListener {
+public class SampleListener implements ApplicationRunner  {
 
-    public SampleListener(ApplicationArguments arguments) {
-        System.out.println("foo: " + arguments.containsOption("foo"));
-        System.out.println("bar: " + arguments.containsOption("bar"));
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("foo: " + args.containsOption("foo"));
+        System.out.println("bar: " + args.containsOption("bar"));
     }
 }
