7eeec6663146fbfab93eff20f3b7d7e43c9ea02e
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-20 21:26:40 +0900

    배너 변경 출력

--- src/main/java/me/facemoktak/springinitapptest/SpringinitapptestApplication.java
+++ src/main/java/me/facemoktak/springinitapptest/SpringinitapptestApplication.java
@@ -2,6 +2,7 @@ package me.facemoktak.springinitapptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringinitapptestApplication {
@@ -9,6 +10,9 @@ public class SpringinitapptestApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringinitapptestApplication.class);
         application.run(args);
+//        new SpringApplicationBuilder()
+//                .main(SpringinitapptestApplication.class)
+//                .run(args);
     }
 
 }
